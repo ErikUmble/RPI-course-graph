71d252cf7fb3aadc33ca6b83479afaac71d2e8a4
Joel McCandless <mail@joelmccandless.com>
2024-09-01 17:38:31 -0400

    add descendants

--- scripts/aggregate_course_data.py
+++ scripts/aggregate_course_data.py
@@ -3,9 +3,11 @@ import json
 
 def aggregate_course_info(prereqs, course_details):
     spaced_course_code = f'{course_details["subj"]} {course_details["crse"]}' # e.g. ADMN 0001
+
     return {
         **course_details,
         'prereqs': list(map(lambda code: code.replace(' ', '-'), prereqs.get(spaced_course_code, {'prereqs': []})['prereqs'])),
+        'descendants': [],
     }
 
 if __name__ == '__main__':
@@ -13,4 +15,12 @@ if __name__ == '__main__':
         catalog = json.load(catalog_file)
         prereqs = json.load(prereqs_file)
         aggregated_data = {dashed_course_code: aggregate_course_info(prereqs, course_details) for dashed_course_code, course_details in catalog.items()}
+        # list immediate descendants
+        for course_code, course_details in aggregated_data.items():
+            for prereq_code in course_details['prereqs']:
+                if prereq_code in aggregated_data:
+                    aggregated_data[prereq_code]['descendants'].append(course_code)
+                else:
+                    print(f'Invalid prereq (course does not exist): {prereq_code}')
+
         aggregated_file.write(json.dumps(aggregated_data, indent=4))
